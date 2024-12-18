4366f0bb68aa9673e5f815f18824a4ea0151deab
Andy Wang <121815777+And1Wang@users.noreply.github.com>
2024-12-18 05:14:54 -0600

    Initial commit from Spring Initializr

---
+++ src/test/java/com/bluecollarhub/BlueCollarHubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bluecollarhub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlueCollarHubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
