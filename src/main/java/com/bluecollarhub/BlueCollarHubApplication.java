f74423c5fa4751e21f4212f52765d6ef2ac41184
Andy Wang <121815777+And1Wang@users.noreply.github.com>
2024-12-23 13:32:41 -0600

    Issues Resolved
    JDK 17 uses the "jakarta" namespace as opposed to "javax".  This resolves the "Not a managed type" error.

--- src/main/java/com/bluecollarhub/BlueCollarHubApplication.java
+++ src/main/java/com/bluecollarhub/BlueCollarHubApplication.java
@@ -2,8 +2,10 @@ package com.bluecollarhub;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+@EnableJpaRepositories("com.bluecollarhub.repository")
 public class BlueCollarHubApplication {
 
 	public static void main(String[] args) {
